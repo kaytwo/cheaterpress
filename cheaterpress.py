49907bb89f7fec07edbd2ce78f41bf8cbaf4b13c
Chris Kanich <ckanich@uic.edu>
2012-12-17 01:02:27 -0600

    interactive game buggy but executes

--- cheaterpress.py
+++ cheaterpress.py
@@ -36,6 +36,7 @@ class Board(object):
     return testboard
 
   def play_word(self,player,spaces):
+    print spaces
     for x,y in spaces:
       space = self.board[x,y]
       if space['owner'] != player and space['defended'] == False:
@@ -47,25 +48,24 @@ class Board(object):
         if owner == 'nobody':
           continue
         # top row
-        if i != 0:
+        if i == 0:
           pass
-        else:
-          if self.board[i-1,j] != owner:
+        elif self.board[i-1,j] != owner:
             self.board[i,j]['defended'] = False
             continue
         # bottom row
-        if i != 4:
+        if i == 4:
           pass
         elif self.board[i+1,j] != owner:
           self.board[i,j]['defended'] = False
           continue
         # left column
-        if j != 0:
+        if j == 0:
           pass
         elif self.board[i,j-1] != owner:
           self.board[i,j]['defended'] = False
           continue
-        if j != 4:
+        if j == 4:
           pass
         elif self.board[i,j+1] != owner:
           self.board[i,j]['defended'] = False
@@ -81,23 +81,15 @@ class Board(object):
         else:
           stats[owner] += 1
     return stats
-  
-  def hilite(string, status):
-      attr = []
-      if status:
-          # green
-          attr.append('32')
-      else:
-          # red
-          attr.append('31')
-      return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), string)
 
   def __str__(self):
     s = []
     for i in range(5):
       for j in range(5):
         fmtstr = '%s'
-        if self.board[i,j]['owner'] == 'player1':
+        if not isatty:
+          fmtstr = '%s'
+        elif self.board[i,j]['owner'] == 'player1':
           fmtstr = '\x1b[35m%s\x1b[0m'
         elif self.board[i,j]['owner'] == 'player2':
           fmtstr = '\x1b[36m%s\x1b[0m'
@@ -106,14 +98,15 @@ class Board(object):
     return ''.join(s)
 
 class Player(object):
-  def __init__(self,c):
+  def __init__(self,c,name):
     self.c = c
+    self.name = name
 
   def choose_next_move(self):
     ''' simple interactive move chooser, sublcass Player to write an AI '''
     word_choice = ''
     while word_choice == '':
-      choose = raw_input("what word do you want to play? ")
+      choose = raw_input("what word do you want to play? ").upper()
       if choose not in self.c.playable_words:
         print "that's not a word :("
       elif choose in self.c.played_words:
@@ -123,17 +116,24 @@ class Player(object):
     # remove ambiguity
     this_move = []
     for letter in word_choice:
-      choices = c.board.find_letter_on_board(letter)
+      choices = self.c.board.find_letter_on_board(letter)
+      for c in this_move:
+        if c in choices:
+          choices.remove(c)
       if len(choices) == 1:
-        this_move.append(choices)
+        this_move.append(choices[0])
       else:
+        pass
         goodmove = ''
-        while goodmove not in choices:
-          print letter
-          print choices
-          goodmove = raw_input("which %s? choices are %s" % (letter,'; '.join([str(x) for x in choices])))
-        this_move.append(goodmove)
-
+        choicestring = '; '.join([str(x) + ': ' + str(choices[x]) for x in range(len(choices))])
+        while goodmove not in range(len(choices)):
+          goodmove = raw_input("which %s? choices are %s " % (letter,choicestring))
+          try:
+            goodmove = int(goodmove)
+          except ValueError:
+            continue
+        this_move.append(choices[int(goodmove)])
+    return this_move,word_choice
       
 
 
@@ -179,11 +179,11 @@ class Cheaterpress(object):
       for i in range(24):
         choices.append(random.choice(Cheaterpress.letters))
         # guarantee game is winnable
-        if 'q' in choices and 'i' not in choices:
-          choices.append('i')
-          random.shuffle(choices)
-        else:
-          choices.append(random.choice(Cheaterpress.letters))
+      if 'q' in choices and 'i' not in choices:
+        choices.append('i')
+        random.shuffle(choices)
+      else:
+        choices.append(random.choice(Cheaterpress.letters))
       boardstring = ''.join(choices)
     self.boardstring = boardstring
     self.board = Board(boardstring)
@@ -203,7 +203,6 @@ class Cheaterpress(object):
     self.played_words = []
 
     self.initialize_wordlist(wordfile)
-    debug('instantiated wordlist of length %d' % len(self.words))
     self.instantiate_game(board)
     debug(self.board)
     t1 = time.time()
@@ -211,23 +210,26 @@ class Cheaterpress(object):
     t2 = time.time()
     # debug('found %d possible words in %.2f seconds' % (len(self.playable_words.keys()),t2-t1))
 
-  def next():
+  def next(self):
     if self.currentplayer== self.player1:
       self.currentplayer = self.player2
     else:
       self.currentplayer = self.player1
 
   def play(self,player1,player2):
-    self.player1 = player1(self)
-    self.player2 = player2(self)
+    self.player1 = player1(self,'player1')
+    self.player2 = player2(self,'player2')
     self.currentplayer = self.player1
     
-    pprint(self.playable_words.keys()[:10])
+    pprint(sorted(self.playable_words.keys(),key=lambda x: 25 - len(x)))
     while not self.board.game_over():
-      self.player1.choose_next_move()
+      print self.board
+      next_move,word_choice = self.currentplayer.choose_next_move()
+      self.board.play_word(self.currentplayer.name,next_move)
+      self.played_words.append(word_choice)
       self.next()
 
 
 if __name__ == '__main__':
-  c = Cheaterpress('words.txt')
+  c = Cheaterpress('words.txt') # ,'FRPGBXTQMTPGRCBYKHSCZFXUA')
   c.play(Player,Player)
