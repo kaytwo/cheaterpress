fbcf51969fdfca3f38bf4e46359848f25ca239c4
Chris Kanich <ckanich@uic.edu>
2012-12-17 21:42:53 -0600

    little clean up, started work on AI

--- cheaterpress.py
+++ cheaterpress.py
@@ -1,3 +1,4 @@
+from itertools import combinations
 import copy
 from collections import defaultdict
 from pprint import pprint
@@ -17,18 +18,30 @@ class Board(object):
     for i in range(5):
       for j in range(5):
         self.board[i,j] = {
+            'i':i,
+            'j':j,
             'letter':boardstring[i*5 + j],
             'owner':'nobody',
             'defended':False
             }
+
+  def tiles(self):
+    for i in range(5):
+      for j in range(5):
+        yield self.board[i,j]
   
   def find_letter_on_board(self,letter):
     hits = []
-    for i in range(5):
-      for j in range(5):
-        if self.board[i,j]['letter'] == letter:
-          hits.append((i,j))
+    for tile in self.tiles():
+      if tile['letter'] == letter:
+        hits.append(tile['i'],tile['j'])
     return hits
+  
+  def score(self):
+    scoreboard = defaultdict(int)
+    for tile in self.tiles():
+      if tile['owner'] != 'nobody':
+        scoreboard[tile['owner']] += 1
 
   def test_play(self,player,spaces):
     testboard = copy.deepcopy(self)
@@ -41,35 +54,36 @@ class Board(object):
       if space['owner'] != player and space['defended'] == False:
         self.board[x,y]['owner'] = player
     # update defense status
-    for i in range(5):
-      for j in range(5):
-        owner = self.board[x,y]['owner']
-        if owner == 'nobody':
+    for tile in self.tiles():
+      i = tile['i']
+      j = tile['j']
+      owner = tile['owner']
+      if owner == 'nobody':
+        continue
+      # top row
+      if i == 0:
+        pass
+      elif self.board[i-1,j]['owner'] != owner:
+          tile['defended'] = False
           continue
-        # top row
-        if i == 0:
-          pass
-        elif self.board[i-1,j]['owner'] != owner:
-            self.board[i,j]['defended'] = False
-            continue
-        # bottom row
-        if i == 4:
-          pass
-        elif self.board[i+1,j]['owner'] != owner:
-          self.board[i,j]['defended'] = False
-          continue
-        # left column
-        if j == 0:
-          pass
-        elif self.board[i,j-1]['owner'] != owner:
-          self.board[i,j]['defended'] = False
-          continue
-        if j == 4:
-          pass
-        elif self.board[i,j+1]['owner'] != owner:
-          self.board[i,j]['defended'] = False
-          continue
-        self.board[i,j]['defended'] = True
+      # bottom row
+      if i == 4:
+        pass
+      elif self.board[i+1,j]['owner'] != owner:
+        tile['defended'] = False
+        continue
+      # left column
+      if j == 0:
+        pass
+      elif self.board[i,j-1]['owner'] != owner:
+        tile['defended'] = False
+        continue
+      if j == 4:
+        pass
+      elif self.board[i,j+1]['owner'] != owner:
+        tile['defended'] = False
+        continue
+      tile['defended'] = True
 
   @staticmethod
   def hilite( is_player1, is_defended):
@@ -84,16 +98,17 @@ class Board(object):
   
   def __str__(self):
     s = []
-    for i in range(5):
-      for j in range(5):
+    ctr = 0
+    for tile in self.tiles():
+      if  isatty and tile['owner'] != 'nobody':
+        fmtstr = self.hilite(tile['owner'] == 'player1',
+                        tile['defended'])
+      else:
         fmtstr = '%s'
-        if not isatty or self.board[i,j]['owner'] == 'nobody':
-          pass
-        else:
-          fmtstr = self.hilite(self.board[i,j]['owner'] == 'player1',
-                          self.board[i,j]['defended'])
-        s.append(fmtstr % self.board[i,j]['letter'])
-      s.append('\n')
+      s.append(fmtstr % tile['letter'])
+      if ctr % 5 == 4:
+        s.append('\n')
+      ctr += 1
     return ''.join(s)
 
 class Player(object):
@@ -134,6 +149,45 @@ class Player(object):
         this_move.append(choices[int(goodmove)])
     return this_move,word_choice
       
+class AIPlayer(Player):
+  def choose_next_move(self):
+    best_plays = []
+    for word in self.c.playable_words:
+      if word not in self.c.played_words:
+        for alternative in self.choose_spaces(playable_word):
+          best_plays.append(alternative)
+    best_plays.sort(key=play_quality,reverse=True)
+    if len(best_plays) == 0:
+      return None
+    return best_plays[0]
+
+  def choose_spaces(self,playable_word,board=None):
+    if board is None:
+      board = self.c.board
+    '''for a given word, spit out all alternative play options
+    as test_play boards for that word.'''
+    this_move = []
+    freq_table = defaultdict(int)
+    alternative_subchoices = []
+    for letter in playable_word:
+      freq_table[letter] += 1
+    for letter in freq_table.keys():
+      choices = board.find_letter_on_board(letter)
+      
+
+
+  @staticmethod
+  def play_quality(next_board):
+    '''create a key for a sort function to compare the quality of diff
+    moves (based on a Board object). this one attempts to maximize area owned,
+    and breaks ties with num defended'''
+    quality = [0,0]
+    for square in self.c.board.board.values():
+      if square['owner'] == self.name:
+        quality[0] += 1
+        if square['defended'] == True:
+          quality[1] += 1
+    return quality[0] * 25 + quality[1]
 
 
 class Cheaterpress(object):
@@ -142,6 +196,7 @@ class Cheaterpress(object):
 
   @classmethod
   def initialize_wordlist(cls,wordfile):
+    assert cls.words or wordfile
     if cls.words is None:
       cls.words = {}
       with open(wordfile) as f:
@@ -149,16 +204,11 @@ class Cheaterpress(object):
           cls.words[line.strip()] = True
  
   def game_over(self):
-    stats = defaultdict(int)
-    for i in range(5):
-      for j in range(5):
-        # should make this less ugly...
-        owner = self.board.board[i,j]['owner']
-        if owner == 'nobody':
-          return False
-        else:
-          stats[owner] += 1
-    self.stats = stats
+    if self.passes == 2:
+      return True
+    for tile in self.board.tiles():
+      if tile['owner'] == 'nobody':
+        return False
     return True
   
 
@@ -189,20 +239,14 @@ class Cheaterpress(object):
   def instantiate_game(self,boardstring):
     if boardstring is None:
       choices = []
-      for i in range(24):
-        choices.append(random.choice(Cheaterpress.letters))
-        # guarantee game is winnable
-      if 'q' in choices and 'i' not in choices:
-        choices.append('i')
-        random.shuffle(choices)
-      else:
+      for i in range(25):
         choices.append(random.choice(Cheaterpress.letters))
       boardstring = ''.join(choices)
     self.boardstring = boardstring
     self.board = Board(boardstring)
 
 
-  def __init__(self,wordfile,board=None):
+  def __init__(self,wordfile=None,board=None):
     '''initialize an individual game of letterpress. 
     
     wordfile gets read in if this is the first game instantiated
@@ -212,38 +256,47 @@ class Cheaterpress(object):
     want to play a specific game, input board as a 25 character string
     reading the board left to right and top to bottom.
     '''
-
+    self.num_players = 2
+    self.players = []
     self.played_words = []
     self.passes = 0
     self.initialize_wordlist(wordfile)
     self.instantiate_game(board)
-    debug(self.board)
+    # debug(self.board)
     t1 = time.time()
     self.find_all_words()
     t2 = time.time()
     # debug('found %d possible words in %.2f seconds' % (len(self.playable_words.keys()),t2-t1))
 
-  def next(self):
-    if self.currentplayer== self.player1:
-      self.currentplayer = self.player2
-    else:
-      self.currentplayer = self.player1
+  def winner(self):
+    stats = self.board.score()
+    return max(stats.items(),lambda x: x[1])[0]
 
-  def play(self,player1,player2):
-    self.player1 = player1(self,'player1')
-    self.player2 = player2(self,'player2')
-    self.currentplayer = self.player1
-    
-    pprint(sorted(self.playable_words.keys(),key=lambda x:  len(x)))
+  def next(self):
+    self.currentplayer = self.players[len(self.played_words) % self.num_players]
+
+  def play(self,players):
+    for playernum in range(len(players)):
+      self.players.append(players[playernum](self,'player' + str(playernum)))
+    self.currentplayer = self.players[0]
+    # pprint(sorted(self.playable_words.keys(),key=lambda x:  len(x)))
     while not self.game_over():
       print self.board
       next_move,word_choice = self.currentplayer.choose_next_move()
-      self.board.play_word(self.currentplayer.name,next_move)
-      self.played_words.append(word_choice)
+      if next_move == None:
+        self.passes += 1
+      else:
+        self.passes = 0
+        self.board.play_word(self.currentplayer.name,next_move)
+        self.played_words.append(word_choice)
       self.next()
-    print self.board
-    print self.stats
+    print self.winner(),'wins.'
 
 if __name__ == '__main__':
-  c = Cheaterpress('words.txt') # ,'FRPGBXTQMTPGRCBYKHSCZFXUA')
-  c.play(Player,Player)
+  numwords = []
+  for i in range(500):
+    c = Cheaterpress('words.txt') # ,'FRPGBXTQMTPGRCBYKHSCZFXUA')
+    num = len(c.playable_words.keys())
+    print "%d possible words" % num
+    numwords.append(num)
+  print 'average: %10.2f, min: %10d, max: %10d' % (float(sum(numwords)/len(numwords)),min(numwords),max(numwords))
